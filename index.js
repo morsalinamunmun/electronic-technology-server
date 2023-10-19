a587ca69ed962a360b23f42f1c75599c87ec5a54
morsalinamunmun <morsalinamunmun@gmail.com>
2023-10-19 13:02:39 +0600

    proucts data server add

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/products', async(req, res)=>{
+        const productInfo = req.body;
+        console.log(productInfo)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
